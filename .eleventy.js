776f99030c270f438c8c7d2a07d621fe62c79f4a
VeejeyPL <wojciech.jurkowski4@gmail.com>
2023-03-06 14:53:35 +0100

    Add js to template

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 // 11ty configuration
 module.exports = (config) => {
   config.addPassthroughCopy("./src/css/");
+  config.addPassthroughCopy("./src/scripts");
   config.addWatchTarget("./src/css/");
+  config.addWatchTarget("./src/scripts");
 
   return {
     dir: {
