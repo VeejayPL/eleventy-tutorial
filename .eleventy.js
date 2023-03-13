4234bbfee46671f3684ae95b301d6d3c6e2ba272
VeejeyPL <wojciech.jurkowski4@gmail.com>
2023-03-13 15:19:13 +0100

    Add shortcode to display year in footer

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = (config) => {
   config.addPassthroughCopy("./src/scripts/");
   config.addWatchTarget("./src/css/");
   config.addWatchTarget("./src/scripts/");
+  config.addShortcode("year", () => `${new Date().getFullYear()}`);
 
   return {
     dir: {
